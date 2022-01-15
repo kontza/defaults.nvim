bef69514fbed07a90008bd97d60d58cf48e6863a
Michael Lingelbach <m.j.lbach@gmail.com>
2022-01-15 09:30:51 -0800

    chore: switch colorscheme
    * Onedark.nvim is my copy of onedark.vim. It is dramatically simplified and removes approximately 30% of the startup cost for kickstart.nvim, while retaining most of the "default" features of onedark.vim

--- init.lua
+++ init.lua
@@ -25,6 +25,7 @@ require('packer').startup(function()
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'danilo-augusto/vim-afterglow'
   -- use 'joshdick/onedark.vim'
+  use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
   use 'itchyny/lightline.vim' -- Fancier statusline
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
@@ -89,11 +90,9 @@ vim.o.smartcase = true
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
---Set colorscheme (order is important here)
+--Set colorscheme
 vim.o.termguicolors = true
-vim.g.afterglow_italic_comments = 1
-vim.cmd [[colorscheme afterglow]]
--- vim.cmd [[colorscheme onedark]]
+vim.cmd [[colorscheme onedark]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -131,11 +130,11 @@ vim.g.indent_blankline_show_trailing_blankline_indent = false
 -- Gitsigns
 require('gitsigns').setup {
   signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
   },
 }
 
